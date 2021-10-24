bbf7fd965f56d7b72da1c0cf377d86fb37f48f92
afcking <owner@afcking.com>
2021-10-24 01:05:48 -0600

    24 oct 2021

--- lib/main.dart
+++ lib/main.dart
@@ -4,50 +4,107 @@ import 'package:flutter/material.dart';
 void main(){
   return runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget {
+String image1= "https://upload.wikimedia.org/wikipedia/commons/9/94/William_%22Mo%22_Cowan.jpg";
+bool isvisible=false;
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.deepOrange,
           title: Text("My First App",
-          style: TextStyle(
-              fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.white
-          ),),
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+            ),),
         ),
 
         body: Center(
-          child: Container(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 15,
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      height: 200,
+                      width: 200,
+                      padding: EdgeInsets.all(5),
+                      child: Image.network(image1),
+                    ),
+                    Container(
+                      height: 200,
+                      width: 200,
+                      padding: EdgeInsets.all(10),
+                      child: Image.network(image1),
+                    ),
+                    Container(
+                      height: 200,
+                      width: 200,
+                      padding: EdgeInsets.all(10),
+                      child: Image.network(image1),
+                    ),
+                    Container(
+                      height: 200,
+                      width: 200,
+                      padding: EdgeInsets.all(10),
+                      child: Image.network(image1),
+                    ),
+                    Container(
+                      height: 200,
+                      width: 200,
+                      padding: EdgeInsets.all(10),
+                      child: Image.network(image1),
+                    ),
+                    Container(
+                      height: 200,
+                      width: 200,
+                      padding: EdgeInsets.all(10),
+                      child: Image.network(image1),
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                    onPressed: (){
-
-                    },
-                    child: Text("Button One")),
-                SizedBox(
-                  height: 15,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+                      isvisible=true;
+                    });
+                  }, 
+                  child: Text("Show Image")),
+              SizedBox(
+                height: 20,
+              ),
+              Visibility(
+                visible: isvisible,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                  ),
+                  height: 200,
+                  width: 200,
+                  child: Image.network(image1,fit: BoxFit.fitHeight),
                 ),
-                Image.network("https://media.contentapi.ea.com/content/dam/gin/images/2021/07/fifa22-key-art.jpg.adapt.crop1x1.767p.jpg")
+              ),
+            ],
 
-
-
-              ],
-            ),
           ),
         ),
       ),
     );
   }
 }
+
